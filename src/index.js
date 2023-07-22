e854a200e3d3f29c666bdb270c778605cc61d0fe
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-07-23 00:41:35 +0300

    6

--- src/index.js
+++ src/index.js
@@ -1,31 +1,58 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// Notify.success('Whooray!');
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '38385479-57784bf7d17c856b0f296bf8b';
 const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+let searchQuery = '';
+let counter = 1;
 
-async function fetchImiges() {
-
-    const BASE_URL = 'https://pixabay.com/api/'; 
-    const API_KEY = '38385479-57784bf7d17c856b0f296bf8b';
-    const query = `${form.elements.searchQuery.value}`
-const options = {
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
-    }
-    const responce = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=${options.image_type}&safesearch=${options.safesearch}`);
+async function fetchImiges() {  
+    let page = 1;
+    const responce = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&safesearch=true&per_page=40&orientation=horizontal&page=${page}`);
     return responce;
 }
 
-
 form.addEventListener('submit', onSubmit);
 
- async function onSubmit(e) {
-     e.preventDefault(); 
-     
-    const query = await e.target.elements.searchQuery.value;
-    console.log(query);
-    fetchImiges().then(data => console.log(data))
-}
+async function onSubmit(e) {
+    e.preventDefault();
+
+    gallery.innerHTML = '';
+    searchQuery = e.target.elements.searchQuery.value.replaceAll(' ', '+');
+    try {
+        const resp = await fetchImiges(searchQuery, counter);
+        const totalHits = resp.data.totalHits;
+
+        if (!totalHits) {
+            throw new Error('Sorry, there are no images matching your search query. Please try again.')
+        }
+
+        Notify.success(`Hooray! We found ${totalHits} images.`);
+
+        const markup = resp.data.hits.map( ({ webformatURL, tags, likes, views, comments, downloads, largeImageURL }) => {
+            return  `<a href="${largeImageURL}" class="photo-card">
+  <div class="img-container"><img class="card-img" src="${webformatURL}" alt="${tags}" loading="lazy" /></div>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> <span class="text-number">${likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b> <span class="text-number">${views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b> <span class="text-number">${comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> <span class="text-number">${downloads}</span>
+    </p>
+  </div>
+</a>`
+        }).join('');
+        gallery.innerHTML = markup;
+    } catch (error) { Notify.failure(error.massage)};
+} 
+ 
